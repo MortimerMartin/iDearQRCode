189ae6dac0f13eef04e310f427e7f2d8395ff11b
Mortimer <Mortimer@prodeimac.local>
2017-05-15 15:31:11 +0800

    Initial Commit

---
+++ iDearQRCode/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iDearQRCode
+//
+//  Created by Mortimer on 17/5/15.
+//  Copyright © 2017年 Mortimer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ iDearQRCode/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iDearQRCode
+//
+//  Created by Mortimer on 17/5/15.
+//  Copyright © 2017年 Mortimer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
